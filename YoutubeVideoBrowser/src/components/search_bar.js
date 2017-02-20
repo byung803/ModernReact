40bf8533accccd071bb2737caf4fb4126942747e
최병민(01045842518) <byung803@gmail.com>
2017-02-20 20:04:57 +0900

    ~1_20

--- YoutubeVideoBrowser/src/components/search_bar.js
+++ YoutubeVideoBrowser/src/components/search_bar.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
-
+//class base component의 컨스트럭터
+//새로운 인스턴의 클래스 만들때마다 호출됨 당연하지 컨스트럭터니까
+//초기화 할때 씀.
+//
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Starting Value' };
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
 
 }
